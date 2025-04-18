223ac9ed454110f2c248cb310cef5b9ad487c1b7
Martina Bernardi <87993191+martina-bernardi@users.noreply.github.com>
2025-04-18 13:21:09 +0200

    clean project

--- authelia/entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-set -e
-
-# Sostituisci la variabile d'ambiente con l'hash reale nel file di configurazione
-sed -i "s|\${ADMIN_PW}|$ADMIN_PW|g" /config/users_database.yml
-
-# Esegui Authelia con i parametri originali
-exec /app/authelia $@
